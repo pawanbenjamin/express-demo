cfc2642d49118af62d64cb1629ee860d679993f0
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-03-21 19:49:52 -0400

    pet data

---
+++ db/cats.js
@@ -0,0 +1,28 @@
+const cats = [
+  {
+    id: 1,
+    name: 'Rick',
+    breed: 'British Shorthair',
+    age: 3,
+  },
+  {
+    id: 2,
+    name: 'Stella',
+    breed: 'Ragdoll',
+    age: 19,
+  },
+  {
+    id: 3,
+    name: 'Ines',
+    breed: 'Norwegian Forest Cat',
+    age: 9,
+  },
+  {
+    id: 4,
+    name: 'Bastet',
+    breed: 'Sphynx',
+    age: 4000,
+  },
+]
+
+module.exports = cats
---
+++ db/dogs.js
@@ -0,0 +1,17 @@
+const dogs = [
+  {
+    id: 1,
+    name: 'Bill',
+    breed: 'Border Collie',
+    age: 12,
+  },
+  { id: 2, name: 'Rosie', breed: 'Great Pyrnees', age: 6 },
+  {
+    id: 3,
+    name: 'Cesar',
+    breed: 'Daschund',
+    age: 8,
+  },
+]
+
+module.exports = dogs
