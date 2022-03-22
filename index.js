a399b7ae5976dfa2d07a53a3db1652a8a1e4355c
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-03-21 23:37:29 -0400

    error handling

--- index.js
+++ index.js
@@ -5,9 +5,39 @@ const app = express()
 
 app.use(volleyball)
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
+
+// * Example of custom auth middleware
+app.use((req, res, next) => {
+  const token = req.header('token')
+  if (!token) {
+    res.status(401).send('You are un-authorized!')
+    return
+  }
+  next()
+})
+
+//* this will crash our server because it is a sync
+// app.get('/', (req, res, next) => {
+//   setTimeout(() => {
+//     console.log('Async code example.')
+//     throw new Error('Hello Error!')
+//   }, 1000)
+// })
 
 app.use('/dogs', require('./routes/dogs'))
+
+app.get('/error', (req, res, next) => {
+  const err = new Error('Ooops')
+  next(err)
+})
+
+app.use((error, req, res, next) => {
+  res.send({ status: 'nope', message: error.message })
+})
+
+// app.get('*', (req, res, next) => {
+//   res.status(404).send('That is not found!')
+// })
 
 app.listen(PORT, () => {
   console.log(`...ayyyyy we're up and running at ${PORT}`)
