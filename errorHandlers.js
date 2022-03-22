9c27d31e39dade260cf0a8cdf8b978cef8268b36
pawanbenjamin <62716484+pawanbenjamin@users.noreply.github.com>
2022-03-21 23:43:24 -0400

    added error handlers.js

---
+++ errorHandlers.js
@@ -0,0 +1,27 @@
+// * Example of custom auth middleware
+app.use((req, res, next) => {
+  const token = req.header('token')
+  if (!token) {
+    res.status(401).send('You are un-authorized!')
+    return
+  }
+  next()
+})
+
+//* this will crash our server because it is a sync
+app.get('/', (req, res, next) => {
+  setTimeout(() => {
+    console.log('Async code example.')
+    throw new Error('Hello Error!')
+  }, 1000)
+})
+
+//* Custom error handler
+app.use((error, req, res, next) => {
+  res.send({ sucess: 'nope', message: error.message })
+})
+
+//* Not technically error handling, but a 'catch all' routes
+app.get('*', (req, res, next) => {
+  res.status(404).send("Sorry, we couldn't find that route!!")
+})
